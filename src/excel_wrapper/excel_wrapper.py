a54ee19c392a52f3f1c4ce46dfa9ad8876be029b
Arul Moondra <amoondra@metamorphsoftware.com>
2015-11-13 15:28:38 -0600

    variables can be refrenced from sheetname and cellname

--- src/excel_wrapper/excel_wrapper.py
+++ src/excel_wrapper/excel_wrapper.py
@@ -3,6 +3,7 @@ import os
 import win32com.client
 import ast
 from xml.etree import ElementTree as ET
+from openpyxl import Workbook
 
 class ExcelWrapper(Component):
     """ An Excel Wrapper """
@@ -47,6 +48,7 @@ class ExcelWrapper(Component):
         self.excelFile = excelFile
         self.xlInstance = None
         self.workbook = None
+        self.xl_sheet = None
         self.ExcelConnectionIsValid = True
         if not os.path.exists(self.excelFile):
             print "Invalid file given"
@@ -62,6 +64,9 @@ class ExcelWrapper(Component):
             else:
                 self.xlInstance = xl
                 self.workbook = xl.Workbooks.Open(self.excelFile)
+                self.workbook = xl.ActiveWorkbook
+
+
     # End __init__
 
     def __del__(self):
@@ -82,6 +87,16 @@ class ExcelWrapper(Component):
 
         return xl
     # End openExcel
+    def letter2num(self,letters, zbase=False):
+        letters = str(letters)
+        letters_up = str(letters.upper())
+        res = 0
+        weight = len(letters_up) - 1
+        for i, c in enumerate(letters_up):
+            res += (ord(c) - 64) * 26**(weight - i)
+        if not zbase:
+            return res
+        return res - 1
 
     def solve_nonlinear(self, params, unknowns, resids):
 
@@ -90,18 +105,40 @@ class ExcelWrapper(Component):
                 self.workbook is None:
             print "Aborted Execution of Bad ExcelWrapper Component Instance"
             return
-
+        wb_py = Workbook()
+        ws = wb_py.active
         wb = self.workbook
         namelist = [x.name for x in wb.Names]
+
+     
 
         for v in self.variables:
             name = v.attrib['name']
 
             if v.attrib['iotype'] == 'in':
+                if 'row' and 'column' in v.attrib:
+                    if 'sheet' in v.attrib:
+                        xl_sheet=self.xlInstance.Sheets(v.attrib['sheet'])
+                    else:
+                        xl_sheet=self.xlInstance.Sheets(1)
+
+                    xl_sheet.Select()
+                    xl_sheet.Cells(v.attrib['row'],self.letter2num(v.attrib['column'])).value = params[name]
+                else:
                     self.xlInstance.Range(wb.Names(name).RefersToLocal).Value = params[name]
             else:
                 try:
-                    excel_value = self.xlInstance.Range(wb.Names(name).RefersToLocal).Value
+                    if 'row' and 'column' in v.attrib:
+                        if 'sheet' in v.attrib:
+                            xl_sheet=self.xlInstance.Sheets(v.attrib['sheet'])
+
+                        else:
+                            xl_sheet=self.xlInstance.Sheets(1)
+
+                        xl_sheet.Select()
+                        excel_value = xl_sheet.Cells(v.attrib['row'],self.letter2num(v.attrib['column'])).value
+                    else:
+                        excel_value = self.xlInstance.Range(wb.Names(name).RefersToLocal).Value
                 except:
                     print 'Cannot retrieve values from the Excel file'
                     if name not in namelist:
