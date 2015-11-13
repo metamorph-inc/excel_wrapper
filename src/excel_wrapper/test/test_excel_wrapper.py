4f76f6f75de3edc9f5364e4f9084ba9bbd68cb55
Arul Moondra <amoondra@metamorphsoftware.com>
2015-11-13 17:27:58 -0600

    test for excel wrapper

--- src/excel_wrapper/test/test_excel_wrapper.py
+++ src/excel_wrapper/test/test_excel_wrapper.py
@@ -4,8 +4,8 @@ import nose
 import sys
 import logging
 import os
+from openmdao.api import IndepVarComp, Group, Problem
 
-from openmdao.util.testutil import assert_raises, assert_rel_error
 
 class ExcelWrapperTestCase(unittest.TestCase):
 
@@ -19,30 +19,23 @@ class ExcelWrapperTestCase(unittest.TestCase):
         pass
         
     def test_ExcelWrapper(self):
-        logging.debug('')
-        logging.debug('test_ExcelWrapper')
+        prob = Problem()
+        root = prob.root = Group()
+        excelFile = r"excel_wrapper_test.xlsx"
+        xmlFile = r"excel_wrapper_test.xml"
+        root.add('ew', ExcelWrapper(excelFile, xmlFile),promotes=['*'])
+        prob.setup()
+        prob.run()
 
-        from excel_wrapper.excel_wrapper import ExcelWrapper
-        test_root = os.path.abspath(os.path.dirname(__file__))
-        excelFile = os.path.join(
-            test_root,
-            "excel_wrapper_test.xlsx"
-        )
+        self.assertEqual((2.1* prob['x']),prob['y'],"Excel Wrapper failed for FLoat values")
+        self.assertEqual((not prob['b']),prob['bout'],"Excel Wrapper failed for Boolean Values")
+        self.assertEqual(prob['s'].lower(),prob['sout'],"Excel Wrapper failed for String values")
+        self.assertEqual(prob['sheet1_in']+100,prob['sheet2_out'],"Excel wrapper fails in multiple sheets")
 
-        xmlFile = os.path.join(
-            test_root,
-            "excel_wrapper_test.xml"
-        )
 
-        ew = ExcelWrapper(excelFile, xmlFile)
-        ew.execute()
-        assert_rel_error(self, ew.y, 2.12345, 0.0001)
-        self.assertEqual(not ew.b, ew.bout, msg='excel_wrapper could not handle a Bool variable')
-        self.assertEqual(ew.s.lower(), ew.sout, msg='excel_wrapper couldn not handle a Str variable.')
-        del(ew)
-        os._exit(1)
+
+
+
         
 if __name__ == "__main__":
-    sys.argv.append('--cover-package=excel_wrapper.')
-    sys.argv.append('--cover-erase')
-    nose.runmodule()
+    unittest.main()
