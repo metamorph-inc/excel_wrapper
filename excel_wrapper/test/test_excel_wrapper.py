173289cfdfdc5168190796f5a4667198ea09e7b9
Arul Moondra <amoondra@metamorphsoftware.com>
2016-02-04 16:17:01 -0600

    extra test for VB component for MACRO

--- excel_wrapper/test/test_excel_wrapper.py
+++ excel_wrapper/test/test_excel_wrapper.py
@@ -17,19 +17,20 @@ class ExcelWrapperTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def _test_ExcelWrapper(self, varFile, inputs={'x': 10, 'b': True, 's': u'aSdF','macroVar': u'macrocheck'}):
+    def _test_ExcelWrapper(self, varFile, inputs={'x': 10, 'b': True, 's': u'aSdF','macroVar': u'macrocheck','macroVB':12}):
         prob = Problem()
         root = prob.root = Group()
         this_dir = os.path.dirname(os.path.abspath(__file__))
         excelFile = os.path.join(this_dir, "excel_wrapper_test.xlsm")
         jsonFile = os.path.join(this_dir, varFile)
-        root.add('ew', ExcelWrapper(excelFile, jsonFile,"Macro5"), promotes=['*'])
+        root.add('ew', ExcelWrapper(excelFile, jsonFile,"Macro5","Sheet3.Transfer_ColA"), promotes=['*'])
         varComp = IndepVarComp(((name, val) for name, val in six.iteritems(inputs)))
         root.add('vc', varComp)
         root.connect('vc.x', 'x')
         root.connect('vc.b', 'b')
         root.connect('vc.s', 's')
         root.connect('vc.macroVar', 'macroVar_in')
+        root.connect('vc.macroVB','macroVB_in')
         prob.setup()
         prob.run()
 
@@ -37,6 +38,7 @@ class ExcelWrapperTestCase(unittest.TestCase):
         self.assertEqual((2.1 * float(inputs['x'])), prob['y'], "Excel Wrapper failed for Float values")
         self.assertEqual(inputs['b'], prob['b'])
         self.assertEqual(prob['macroVar_in'],(prob['macroVar_out']))
+        self.assertEqual(3 * int(prob['macroVB_in'])-1,int(prob['macroVB_out']))
         self.assertEqual(bool(prob['b']), not prob['bout'])
         self.assertEqual(inputs['s'], prob['s'])
         self.assertEqual(prob['s'].lower(), prob['sout'], "Excel Wrapper failed for String values")
@@ -46,10 +48,10 @@ class ExcelWrapperTestCase(unittest.TestCase):
         return self._test_ExcelWrapper("testjson_1.json")
 
     def test_ExcelWrapperJson2(self):
-        return self._test_ExcelWrapper("testjson_1.json", inputs={'x': -10, 'b': False, 's': u'TEST','macroVar': u'macroTest'})
+        return self._test_ExcelWrapper("testjson_1.json", inputs={'x': -10, 'b': False, 's': u'TEST','macroVar': u'macroTest','macroVB':12})
 
     def test_ExcelWrapperXml(self):
-        return self._test_ExcelWrapper("excel_wrapper_test.xml")
+       return self._test_ExcelWrapper("excel_wrapper_test.xml")
 
 
 if __name__ == "__main__":
